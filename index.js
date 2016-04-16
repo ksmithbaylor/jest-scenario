43de6d89ee2c756fccb6bf607b501d37e3099c12
Kevin Smith <ksmithbaylor@gmail.com>
2016-04-16 11:59:20 -0400

    Rename config to testCases for clarity

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var keys = Object.keys || require('object-keys');
 
-module.exports = function scenario(test, prefix, config, testBody) {
-  keys(config).forEach(function (key) {
+module.exports = function scenario(test, prefix, testCases, testBody) {
+  keys(testCases).forEach(function (key) {
     test(prefix + key, function (t) {
-      testBody(t, config[key]);
+      testBody(t, testCases[key]);
     });
   });
 }
